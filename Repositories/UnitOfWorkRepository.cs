c6f282af6843afa1eb1109a06de2a6107bd69b82
phandb72 <phandb72@gmail.com>
2021-11-27 10:55:01 -0600

    Build UnitOfWorkRepository

---
+++ Repositories/UnitOfWorkRepository.cs
@@ -0,0 +1,43 @@
+﻿using EmployeeCapibilityDemonstration.Data;
+using EmployeeCapibilityDemonstration.Interfaces;
+
+namespace EmployeeCapibilityDemonstration.Repositories
+{
+    public class UnitOfWorkRepository : IUnitOfWorkRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private IEmployeeRepository _employeeRepo;
+        private IMethodRepository _methodRepo;
+        private ICategoryRepository _categoryRepo;
+
+
+        public UnitOfWorkRepository(ApplicationDbContext context)
+
+        {
+            _context = context;
+
+        }
+        public IEmployeeRepository Employee => _employeeRepo = _employeeRepo ?? new EmployeeRepository(_context);
+
+        public IMethodRepository Method
+        {
+            get
+            {
+                return _methodRepo = _methodRepo ?? new MethodRepository(_context);   
+            }
+        }
+
+        public ICategoryRepository Category
+        {
+            get
+            {
+                return _categoryRepo = _categoryRepo ?? new CategoryRepository(_context);
+            }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
