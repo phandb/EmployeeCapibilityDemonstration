d5f30061f54779c4dcc35f156d097195de9225b3
phandb72 <phandb72@gmail.com>
2021-11-27 22:42:04 -0600

    Create View Models

---
+++ ViewModels/Category/CategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeCapibilityDemonstration.ViewModels.Category
+{
+    public class CategoryViewModel
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [Display(Name="Category")]
+        public string Name { get; set; }
+        //public List<CategoryViewModel> EmployeesByCategory { get; set; }
+    }
+}
---
+++ ViewModels/Method/MethodViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeCapibilityDemonstration.ViewModels.Method
+{
+    public class MethodViewModel
+    {
+        [Key]
+        public string MethodId { get; set; }
+
+        [Required]
+        [Display(Name = "Method Name")]
+        public string Name { get; set; }
+
+        // public List<MethodViewModel> EmployeesByMethod { get; set; }
+    }
+}
